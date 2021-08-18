12fb7c328cf69313a103dd9452b8382ecde49e80
Iyurii Ilinskii <Ilinskiy.ya@gmail.com>
2021-08-18 18:58:53 +0200

    master

--- js/main.js
+++ js/main.js
@@ -163,30 +163,44 @@ ymaps.ready(init);
 
 //OPC
 
-/*const sections = $('section');
+const sections = $('section');
 const display = $(".maincontent");
+
+let inScroll = false;
 
 sections.first().addClass("active");
 
+
+
 const performTransition = sectionEq => {
-    const position = sectionEq * -50;
+    if(inScroll == false) {
+        inScroll = true;
+        const position = sectionEq * -100;
 
-    display.css ({
-        transform: `translateY(${position}%)`
-    });
-}
+        display.css ({
+            transform: `translateY(${position}%)`
+        });
+    
+        sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
 
-const scrollVivport = direction => {
-    const activeSection = section.filter(".active");
+        setTimeout(()=>{
+            inScroll = false;
+
+        }, 1300);
+    }   
+};
+
+const scrollViewport = direction => {
+    const activeSection = sections.filter(".active");
     const nextSection = activeSection.next();
     const prevSection = activeSection.prev();
 
-    if (direction == "next") {
-
+    if (direction == "next" && nextSection.length) {
+        performTransition(nextSection.index())
     }
 
-    if (direction == "prev") {
-
+    if (direction == "prev" && nextSection.length) {
+        performTransition(prevSection.index())
     }
 }
 
@@ -194,11 +208,39 @@ $(window).on("wheel", e => {
     const deltaY = e.originalEvent.deltaY;
 
     if (deltaY > 0) {
-        performTransition(2);
+        //performTransition(2);
+        scrollViewport("next");
     }
 
     if (deltaY < 0) {
-
+        scrollViewport("prev");
     }
-    console.log(deltaY);
-})*/
+    //console.log(deltaY);
+});
+
+$(window).on("keydown", (e) => {
+
+    const tagName = e.target.tagName.toLowerCase();
+
+    if (tagName != "input" && tagName !=  "textarea") {
+        switch (e.keyCode) {
+            case 38:
+            scrollViewport ("prev");
+            break;
+    
+            case 40:
+                scrollViewport ("next");
+                break;
+        }
+    }    
+});
+
+$("[data-scroll-to]").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
+    const reqSection = $(`[data-section-id=${target}]`);
+
+    performTransition (reqSection.index());
+})
