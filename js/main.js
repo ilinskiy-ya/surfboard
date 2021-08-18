2082b4e972b3d916690ee6e8da27e07f3a57acc0
Iyurii Ilinskii <Ilinskiy.ya@gmail.com>
2021-08-18 22:13:05 +0200

    master

--- js/main.js
+++ js/main.js
@@ -177,14 +177,31 @@ const performTransition = sectionEq => {
         inScroll = true;
         const position = sectionEq * -100;
 
+        const currentSection = sections.eq(sectionEq);
+            const menuTheme = currentSection.attr("data-sidemenu-theme");
+            const sideMenu = $(".fixed-menu")
+            if (menuTheme == "black") {
+                sideMenu.addClass("fixed-menu--shadowed");
+            } else {
+                sideMenu.removeClass("fixed-menu--shadowed");
+            }
+
         display.css ({
             transform: `translateY(${position}%)`
         });
     
         sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
 
+        sideMenu
+        .find (".fixed-menu__item")
+        .eq(sectionEq)
+        .addClass("fixed-menu__item--active")
+        .siblings()
+        .removeClass("fixed-menu__item--active");
+
         setTimeout(()=>{
-            inScroll = false;
+            inScroll = false;                
+        
 
         }, 1300);
     }   
